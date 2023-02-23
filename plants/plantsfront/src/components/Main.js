8cec91d067ac7af9e9559806c382005962140def
unknown <wiktor.dabrowa98@gmail.com>
2023-02-23 15:53:49 +0100

    dynamic search filtering

--- plants/plantsfront/src/components/Main.js
+++ plants/plantsfront/src/components/Main.js
@@ -1,72 +1,24 @@
 import React from "react";
+import Search from './Search'
 
 export default function Main() {
     let [test,setTest] = React.useState([])
-    let [query,setQuery] = React.useState([])
-    console.log('Main: ')
-    console.log(query[0])
-    async function getData() {
-        console.log('sent request')
-        fetch('http://127.0.0.1:8000/external_api',
-        {
-            method: 'POST'
-        })
-        .then((response) => response.json())
-        .then((data) => console.log(data))
-    }   
     function internal_data(){
         console.log('Internal data:')
         fetch('http://127.0.0.1:8000/plants')
         .then((response) => response.json())
         .then((data) => console.log(data))
     }
-    function search(event){
-        // Wyszukiwanie: 
-            // gdy input.length === 3 v 4 v 5 
-            //      -> wyslij zapytanie do serwera
-            //      -> response = array możliwych opcji
-            //      -> response do state
-            // gdy input.lenght > 3 v 4 v 5
-            //      -> przeszukaj response 
-            // response.lenght = {3=>100,4=>35}
-        if (event.target.value.length < 3) {
-            // Do nothing
-        }
-        else if ( event.target.value.length === 3) {
-            // Freeze until response???
-            console.log(event.target.value)
-            // Send request to server and save data
-            let data = new FormData()
-            data.append('json', JSON.stringify({data: event.target.value}))
-            fetch('http://127.0.0.1:8000/external_api',{
-                method: 'POST',
-                body: data,
-            })
-            .then((response) => response.json())
-            .then((data) => {
-                console.log(data.data)
-                setQuery(data.data)
-            })
-        } else {
-            console.log('wiecej niz 4')
-            query.filter(
-                // Common name
-                // Other name
-                // Scientific name
-            ) 
-            
-        }
-    }
+    // Transfer to Seperate Component:
     
 
     return (
         <div>
             <div>
                 Main
-                <input className='search' placeholder="Look for your plant..." onKeyUp={search}></input>
             </div>
             <div>
-                .....
+                <Search />
             </div>
         </div>
     )
---
+++ plants/plantsfront/src/components/Search.js
@@ -0,0 +1,54 @@
+import React from "react"
+
+export default function Search() {
+    // State: 
+    let [plants,setPlants] = React.useState([])
+    let [filtered_plants, setFilteredPlants] = React.useState([])
+    console.log(filtered_plants)
+
+    function search(event){
+        // Starts search if query is longer than 3 characters
+        if (event.target.value.length < 3) {
+            // Do nothing
+        }
+        else if ( event.target.value.length === 3) {
+            // Freeze until response???
+            console.log(event.target.value)
+            // Send request to server and save data
+            let data = new FormData()
+            data.append('json', JSON.stringify({data: event.target.value}))
+            fetch('http://127.0.0.1:8000/external_api',{
+                method: 'POST',
+                body: data,
+            })
+            .then((response) => response.json())
+            .then((data) => {
+                console.log(data.data)
+                setPlants(data.data)
+            })
+        } else {
+            let query = event.target.value.toLowerCase()
+            const contains_query = (element) => element.toLowerCase().includes(query)
+            setFilteredPlants(plants.filter((obj) => {
+                if (obj.common_name.toLowerCase().includes(query)) {
+                    return true
+                } else if (obj.scientific_name.some(contains_query)) {
+                    return true
+                } else if (obj.other_name !== null) {
+                    if (obj.other_name.some(contains_query)) {
+                        return true
+                    }
+                }
+            })
+            ) 
+            
+        }
+    }
+
+    return (
+        <div>
+            SEARCH BAR
+            <input onKeyUp={search} placeholder='Look for your plant...'></input>    
+        </div>
+    )
+}
