a0d7a61c0a08ea040c22f022499d973ae73daedb
unknown <wiktor.dabrowa98@gmail.com>
2023-02-28 19:19:02 +0100

    Plant component added

--- plants/plants/settings.py
+++ plants/plants/settings.py
@@ -131,3 +131,4 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_ROOT = BASE_DIR / 'photos/'
+MEDIA_URL = '/media/'
--- plants/plantsfront/src/components/Rooms.js
+++ plants/plantsfront/src/components/Rooms.js
@@ -1,4 +1,6 @@
 import React from "react";
+import Plant from "./Plant";
+
 
 export default function Rooms() {
     const [rooms,setRooms] = React.useState([])
@@ -17,30 +19,20 @@ export default function Rooms() {
     )
     function toggleShow(id){
         let container = document.querySelector(`#room_container_${id}`)
-        container.querySelector('.room_plants').classList.toggle('not_shown')   
+        container.classList.toggle('not_shown')   
     }
     const containers = rooms.map((room) => {
         let room_plants = plants.map((plant) => {
             if (plant.localization === room.id) {
                 return (
-                    <div className='plant' key={plant.id}>
-                        {plant.name}
-                        <br/>
-                        {plant.last_watering}
-                        <br/>
-                        {plant.water_how_often}
-                        <br/>
-                        {plant.localization}
-                        <br/>
-                        {plant.sunlight}
-                    </div>
+                    <Plant plant={plant} key={plant.id}/>
                 )
             }
         })
         return (
-            <div className='room_container' key={room.id} id={`room_container_${room.id}`}>
-                <h2>{room.name}</h2>
-                <button onClick={() => toggleShow(room.id)} className='hide_btn'>
+            <div className='room_container not_shown' key={room.id} id={`room_container_${room.id}`}>
+                <h2 onClick={() => toggleShow(room.id)}>{room.name}</h2>
+                <button className='hide_btn'>
                     <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-caret-down-fill" viewBox="0 0 16 16">
                         <path d="M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z"/>
                     </svg>
